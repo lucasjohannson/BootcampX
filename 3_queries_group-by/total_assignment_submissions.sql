ca78a9ac21622812ec47994e02f262f329eb97f3
lucas <lucas.johannson1@ucalgary.ca>
2021-06-22 21:00:03 +0000

    added teachers and assistance request tables and fake data

--- 3_queries_group-by/total_assignment_submissions.sql
+++ 3_queries_group-by/total_assignment_submissions.sql
@@ -1,7 +1,18 @@
 
-SELECT cohorts.name as cohort, count(assignment_submissions.*) AS total_submissions 
-FROM cohorts
-JOIN students ON cohorts.id = cohort_id
-GROUP BY cohort_name 
-HAVING count(students.*) >= 18
-ORDER BY student_count;
+-- SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- limit 5;
+
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- GROUP BY cohorts.name
+-- ORDER BY total_submissions DESC;
+
+SELECT cohorts.name AS cohort, count(assignment_submissions.id) AS total_submissions
+from assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohort_id = cohorts.id
+GROUP BY cohort;
+
+
