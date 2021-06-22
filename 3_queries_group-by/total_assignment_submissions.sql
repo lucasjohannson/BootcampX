2fcf6b5fbde6f498c5cc1e355d9ef7e31e26b82b
lucas <lucas.johannson1@ucalgary.ca>
2021-06-22 22:14:54 +0000

    4_queries activities

--- 3_queries_group-by/total_assignment_submissions.sql
+++ 3_queries_group-by/total_assignment_submissions.sql
@@ -1,18 +1,8 @@
 
--- SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
--- FROM assignment_submissions
--- JOIN students ON students.id = student_id
--- JOIN cohorts ON cohorts.id = cohort_id
--- limit 5;
-
--- JOIN cohorts ON cohorts.id = cohort_id
--- GROUP BY cohorts.name
--- ORDER BY total_submissions DESC;
-
-SELECT cohorts.name AS cohort, count(assignment_submissions.id) AS total_submissions
-from assignment_submissions
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
 JOIN students ON students.id = student_id
-JOIN cohorts ON cohort_id = cohorts.id
-GROUP BY cohort;
-
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
 
